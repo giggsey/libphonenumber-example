d56e74f1e429417e9d43752ff3ee893979ca5a59
Joshua Gigg <giggsey@gmail.com>
2013-08-12 09:33:58 +0100

    Add version information to the footer

--- index.php
+++ index.php
@@ -7,6 +7,19 @@ $twig = new Twig_Environment(new Twig_Loader_Filesystem(__DIR__ . '/templates/')
 
 $twig->addGlobal('baseurl', $_SERVER['SCRIPT_NAME']);
 
+/* Parse installed.json to get versions */
+$versionInfo = json_decode(file_get_contents(__DIR__ . '/vendor/composer/installed.json'), true);
+
+$versionDetails = array();
+
+foreach ($versionInfo as $r) {
+    $versionDetails[$r['name']] = array(
+        'version' => $r['version'],
+        'actual' => $r['dist']['reference']
+    );
+}
+
+$twig->addGlobal('version', $versionDetails);
 
 $twig->addFunction('get_class', new Twig_Function_Function('get_class'));
 
